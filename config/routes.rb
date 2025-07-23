e33cc705171152adfef19310c021bbebb0918f9d
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-07-23 15:36:26 -0400

    Use resources for articles rather than explicit routes. Shorthand index erb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "articles/:id", to: "articles#show"
+  # For the purposes of incremental training, these were included.
+  # However, further along in the exercise, they were replaced by the below.
+
+  resources :articles
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
